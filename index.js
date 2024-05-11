3ea7a1cc674537e010e72ab4263e7048154c059e
hrmrakib <hrmrakibs@gmail.com>
2024-05-11 11:45:31 +0600

    add route for save user add food item and get her/his food item based on userEmail

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 
 // mongodb database connection
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.dmwxvyo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,11 +33,38 @@ async function run() {
 
     const batabase = client.db("foodTable");
     const userCollection = batabase.collection("users");
+    const myFoodCollection = batabase.collection("myFoods");
 
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log(user);
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/allFood", async (req, res) => {
+      const allFood = await myFoodCollection.find().toArray();
+      res.send(allFood);
+    });
+
+    app.get("/single-food/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await myFoodCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/my-added-food/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const cursor = myFoodCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/myFoods", async (req, res) => {
+      const food = req.body;
+      const result = await myFoodCollection.insertOne(food);
       res.send(result);
     });
 
